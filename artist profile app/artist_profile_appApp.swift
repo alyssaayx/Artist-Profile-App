2875e576b591e67f77b14121da3768a8cac2e832
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-08-25 00:13:09 +0800

    Initial Commit

---
+++ artist profile app/artist_profile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  artist_profile_appApp.swift
+//  artist profile app
+//
+//  Created by rgs on 25/8/21.
+//
+
+import SwiftUI
+
+@main
+struct artist_profile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
