46a926a94e9e005a8e94cc79c866e1a7b60deaf9
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-08-25 00:43:36 +0800

    Added album names and lists. Huge padding between icons and top area

--- artist profile app/ContentView.swift
+++ artist profile app/ContentView.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "star")
-                    .foregroundColor(Color.pink)
-                Image(systemName: "text.badge.plus")
-                    .foregroundColor(Color.pink)
-            }
-            Image(systemName: "artistprofile1")
-            List {
-                // add stuff taught in previous swift accel lesson - lists and small headings of lists
-                // also find out why image won't display
+        NavigationView {
+            VStack {
+                HStack {
+                    Image(systemName: "star")
+                        .foregroundColor(Color.pink)
+                    Image(systemName: "text.badge.plus")
+                        .foregroundColor(Color.pink)
                 }
+                
+                List {
+                    Text("PRODUCER")
+                        .foregroundColor(Color.pink)
+                    Text("LIT - The 4th Album")
+                        .foregroundColor(Color.pink)
+                    Text("NAMANANA - The 3rd Album")
+                        .foregroundColor(Color.pink)
+                    Text("LAY 02 SHEEP")
+                        .foregroundColor(Color.pink)
+                    Text("LOSE CONTROL - The 1st Mini Album")
+                        .foregroundColor(Color.pink)
+                    // add stuff taught in previous swift accel lesson - lists and small headings of lists
+                    // also find out why image won't display
+                    }
             }
+        }
         }
     }
 
