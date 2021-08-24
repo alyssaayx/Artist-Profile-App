8524ac20f4171dbf7c0b264a9c60c725a985c527
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-08-25 00:29:50 +0800

    Built the main icons for add to playlist and starred. Image won't display.

--- artist profile app/ContentView.swift
+++ artist profile app/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Image(systemName: "star")
+                    .foregroundColor(Color.pink)
+                Image(systemName: "text.badge.plus")
+                    .foregroundColor(Color.pink)
+            }
+            Image(systemName: "artistprofile1")
+            List {
+                // add stuff taught in previous swift accel lesson - lists and small headings of lists
+                // also find out why image won't display
+                }
+            }
+        }
     }
-}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
